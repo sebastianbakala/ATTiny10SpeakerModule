d32cf931ff9f14a61fec705da642c966a3419898
sebastianbakala <sebastianbakala@users.noreply.github.com>
2017-09-09 21:17:26 +0200

    Update playSound.h

--- ArduinoTestProgram/playSound.h
+++ ArduinoTestProgram/playSound.h
@@ -1,22 +1,21 @@
- /* ATTiny Sound Generator Test sketch for Arduino Due
-  * Created: 31.07.2017 11:14:31
-  * Author : Tomasz Jaworski
-  * Modified: 20.08.2017 16:36:18
-  * Author: Sebastian Bąkała
-  * ATTiny Sound Generator v0.8
-  */
+/* ATTiny Sound Generator Test sketch for Arduino Due
+
+* Created: 31.07.2017 11:14:31
+* Author : Tomasz Jaworski
+* Modified: 09.09.2017 21:14:38
+* Author: Sebastian Bąkała
+
+* ATTiny Sound Generator v0.8
+*/
 
 #ifndef SOUND_CLOCK
-#define SOUND_CLOCK
+	#define SOUND_CLOCK 2
+	#define SOUND_DATA 3
+	#define f0 440 //Hz
+	#define a 1.059463094359 //2^(1/12)
 
-  #define SOUND_CLOCK 2
-  #define SOUND_DATA 3
-  #define f0 440 //Hz
-  #define a 1.059463094359 //2^(1/12)
-
-  void soundInit(void);
-  void sound(int32_t, uint8_t);
-  void noSound(void);
-  void playSound(const char *);
-
+	void soundInit(void);
+	void sound(uint16_t, uint8_t);
+	void noSound(uint8_t);
+	void playSound(const char *);
 #endif
